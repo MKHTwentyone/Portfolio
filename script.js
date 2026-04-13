dd2b24288ab79dec6cae0a44cede4417a257424c
designer <keishamdesigner@gmail.com>
2026-04-14 02:51:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = "UI/UX Designer | Graphic Designer";
+let i = 0;
+
+function typing(){
+  if(i < text.length){
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+
+typing();
